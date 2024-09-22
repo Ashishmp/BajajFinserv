3d8cbf994642deb2a843c9edbb4952f62fc7c028
Ashish Ranjan <91370291+Ashishmp@users.noreply.github.com>
2024-09-22 18:14:19 +0530

    Update page.tsx

--- bajaj_finserv-main/app/page.tsx
+++ bajaj_finserv-main/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div>
       <h1 className="text-4xl px-4 font-bold mb-4 text-white text-center">Bajaj Finserv Health Challenge</h1>
-      <h1 className="text-2xl px-4 font-bold mb-4 text-white text-center">(Arjun Malhotra 21BBS0110)</h1>
+      <h1 className="text-2xl px-4 font-bold mb-4 text-white text-center">(Ashish Ranjan RA2111029010048)</h1>
       <div className="w-full flex flex-col space-y-4 justify-center items-center py-3 pb-8">
         <Link href={"https://github.com/Ashishmp/BajajFinserv"} className="w-fit px-4 rounded-lg py-2 flex justify-center items-center bg-purple-500 text-white">
           Github Repo
